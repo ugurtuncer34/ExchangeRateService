5891e9682d4c96f45ecdae4902d5ab20621f876b
theugurtuncer <ugurtuncer93@icloud.com>
2026-07-11 22:04:00 +0300

    configure OpenTelemetry tracer provider and integrate gRPC stats handler for distributed tracing

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -13,9 +15,61 @@ import (
 
 	mygrpc "exchangerateservice/internal/grpc" // special name to prevent name conflict
 	"exchangerateservice/internal/grpc/pb"
+
+	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 )
 
+// OPENTELEMETRY Tracer
+func initTracer() (*sdktrace.TracerProvider, error) {
+	endpoint := os.Getenv("OTLP_ENDPOINT")
+	if endpoint == "" {
+		endpoint = "localhost:4317"
+	}
+
+	exporter, err := otlptracegrpc.New(context.Background(),
+		otlptracegrpc.WithEndpoint(endpoint),
+		otlptracegrpc.WithInsecure(), // no password for local test
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := resource.Merge(
+		resource.Default(),
+		resource.NewWithAttributes(
+			"",
+			semconv.ServiceName("FamilyFinance.ExchangeRate"), // name to appear in Jaeger
+		),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exporter),
+		sdktrace.WithResource(res),
+	)
+	otel.SetTracerProvider(tp)
+	return tp, nil
+}
+
 func main(){
+	// start OTel Tracer and clear on close
+	tp, err := initTracer()
+	if err != nil {
+		log.Fatalf("Failed to initialize OpenTelemetry tracer: %v", err)
+	}
+	defer func() {
+		if err := tp.Shutdown(context.Background()); err != nil {
+			log.Printf("Error shutting down tracer: %v", err)
+		}
+	}()
+
 	// wake up common cache system and background worker
 	cache := rates.NewRateCache()
 	rates.StartProactiveCache(cache)
@@ -29,7 +83,9 @@ func main(){
 		log.Fatalf("Failed to listen for gRPC: %v", err)
 	}
 
-	grpcServer := grpc.NewServer() // create gRPC server object
+	grpcServer := grpc.NewServer( // create gRPC server object and inject OTel Handler
+		grpc.StatsHandler(otelgrpc.NewServerHandler()),
+	) 
 	// create GrpcServer and give cache inside
 	myGrpcService := &mygrpc.GrpcServer{
 		Cache: cache,
